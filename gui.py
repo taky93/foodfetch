77f592ade7b38a4a25b85f1fa647b42b6ebc60de
taky93 <takybre93@gmail.com>
2023-07-26 21:03:34 +0200

    making progress with gui :D

--- gui.py
+++ gui.py
@@ -1,13 +1,16 @@
 #tkinter boilerplate code
 import tkinter as tk
-import Api.food as api
+from tkinter import ttk
+from Api.food import Nosalty
 window = tk.Tk()
-window.geometry("800x600")
+window.geometry("300x200")
 window.title("Nosalty API")
+api = Nosalty()
 
 class App:
     def __init__(self, master):
         self.master = master
+
         self.label = tk.Label(master, text="Nosalty API",font=("Arial", 25))
         self.label.pack()
         self.button = tk.Button(master, text="Quit", command=master.quit)
@@ -18,10 +21,23 @@ class App:
         self.fetch.pack()
         
     def fetchData(self):
-        self.fetch = tk.Label(self.master, text=str(api.Nosalty().fetchFood(self.food.get())))
+        titles ,url = api.fetch_data(self.food.get())
+        table_window = tk.Tk()
+        self.fetch = ttk.Treeview(table_window)
+        self.fetch["columns"] = ("Title", "Url")
+        self.fetch.heading("Title", text="Title")
+        self.fetch.heading("Url", text="Url")
+        
+        self.fetch.column("Title", width=400, minwidth=200, stretch=tk.NO)
+        self.fetch.column("Url", width=400, minwidth=200, stretch=tk.NO)
+        
+
+ 
+            
         self.fetch.pack()
+        
     def run(self):
         self.master.mainloop()
         
-app = App(window)
+app = App(window,)
 app.run()
