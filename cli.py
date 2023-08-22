158f82f2ffa124d13c16e7ff6ac9630fbe35c358
taky93 <takybre93@gmail.com>
2023-08-22 20:16:32 +0200

    save function started

--- cli.py
+++ cli.py
@@ -1,8 +1,7 @@
 from Api.food import Nosalty
 import logging
 import argparse
-
-
+import pandas as pd
 class Cli:
     def __init__(self):
         self.app = Nosalty()
@@ -12,6 +11,7 @@ class Cli:
         self.parser.add_argument('-f', '--fetch', metavar="", help="Fetch list of food")
         self.parser.add_argument(
          '-s', '--single', help="Pick single food", action='store_true')
+        self.parser.add_argument('-sv' , '--save', help='Save to csv',action='store_true')
         self.args = self.parser.parse_args()
 
     def run(self):
@@ -21,6 +21,16 @@ class Cli:
             print(title)
             for i in ingredients:
                 print(i)
+            
+            
+            if self.args.save:
+                txt_file = "".join((str(self.args.fetch),'.txt'))
+                with open(txt_file,"w",encoding='utf-8') as file:
+                    file.write(title + "\n")
+                    for i in ingredients:
+                        file.write(i+'\n')
+                print("Saved as " + txt_file)   
+                
 
         else:
             print(self.app.fetchFood(args=self.args.fetch))
